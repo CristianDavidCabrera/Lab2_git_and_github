c3c3ef506938c946eac2406c09542074b25cb587
Reboot Student <cristiancabrera.1985@gmail.com>
2024-09-05 09:48:21 +0100

    Se comenta todo

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -1,7 +1,8 @@
+/************************************** */
 // Muy bonito pero esto no es JavaScript!!
+/************************************** */
 
-
-TTTTTT H   H   A   N   N  K   K     Y   Y  OOO  U   U
+/* TTTTTT H   H   A   N   N  K   K     Y   Y  OOO  U   U
   T   H   H  A A  NN  N  K  K       Y Y  O   O U   U
   T   HHHHH AAAAA N N N  KKK         Y   O   O U   U
   T   H   H A   A N  NN  K  K        Y   O   O U   U
@@ -9,7 +10,6 @@ TTTTTT H   H   A   N   N  K   K     Y   Y  OOO  U   U
 
 
 
-  TO CREMAAAAA
-MAS CAMBIOS
- /************************************** */
+CAMBIO
+MAS CAMBIOS */
 
